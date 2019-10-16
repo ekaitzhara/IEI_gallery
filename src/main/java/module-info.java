3fb9b341847e045aa1d3e6af56c797b678e1d244
anderdu <aduran022@ikale.ehu.eus>
2019-10-16 12:22:34 +0200

    anderren lehen aldaketak 2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,8 @@ open module DASI {
   requires javafx.controls;
   requires javafx.graphics;
   requires javafx.fxml;
+  requires java.desktop;
+  requires flickr4java;
+  requires scribejava.core;
   exports interfazeak;
 }
