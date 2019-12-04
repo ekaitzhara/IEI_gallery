59a21d0d9b899487aa84ecff2cd150e35e862a23
ekaitzhara <ekaitzhara16@gmail.com>
2019-12-04 13:22:05 +0100

    sqlite ondo sartu erabiltzailea

--- src/main/java/ehu/isad/db/ErabiltzaileDBKud.java
+++ src/main/java/ehu/isad/db/ErabiltzaileDBKud.java
@@ -21,8 +21,10 @@ public class ErabiltzaileDBKud {
         idErab = id;
         DBKudeatzaile dbKud = DBKudeatzaile.getInstantzia();
         String query = "INSERT INTO Erabiltzailea(idErabiltzailea, izena) VALUES('"+ id +"', '"+ izen +"')";
+        //query = "INSERT INTO Erabiltzailea(idErabiltzailea, izena) VALUES('dsubda@12', 'asas')";
         dbKud.execSQL(query);
 
+        System.out.println(id + " , "  + izen + " sartuta datu basean");
     }
 
     public static String getIdErab() {
