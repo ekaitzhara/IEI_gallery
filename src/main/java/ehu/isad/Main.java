31b807dacaf0650cb560b119bdadb9e66f40009b
ekaitzhara <ekaitzhara16@gmail.com>
2020-03-18 17:49:32 +0100

    Primeras clases y pantalla principal

---
+++ src/main/java/ehu/isad/Main.java
@@ -0,0 +1,27 @@
+package ehu.isad;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+
+  }
+
+  private void pantailakKargatu() throws IOException {
+
+
+  }
+
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+}
